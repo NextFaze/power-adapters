7a8f78acf922c4fc3d9ee454891dad01687182fa
Ben Tilbrook <btilbrook@nextfaze.com>
2014-09-10 15:53:22 +0930

    Added javadoc to Mapper

--- databind/src/main/java/com/nextfaze/databind/Mapper.java
+++ databind/src/main/java/com/nextfaze/databind/Mapper.java
@@ -1,8 +1,16 @@
 package com.nextfaze.databind;
 
+import android.view.View;
 import lombok.NonNull;
 
+/** Used to determine which {@link Binder} should be used to bind a data item to a {@link View}. */
 public interface Mapper {
+    /**
+     * Given a data item and position, returns the appropriate binder that should be used to bind it to a {@link View}.
+     * @param item The data object to be bound.
+     * @param position The position of the object in the data set.
+     * @return A binder which will be used to bind this object to a view, never {@code null}.
+     */
     @NonNull
     Binder getBinder(@NonNull Object item, int position);
 }
