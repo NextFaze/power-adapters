72c42df01612aa3b32cce869b9408163b211d805
Ben Tilbrook <btilbrook@nextfaze.com>
2015-08-07 12:20:13 +0930

    DataAdapter only registers with it's Data while any DataSetObservers are registered with it

--- async-data/src/main/java/com/nextfaze/asyncdata/DataAdapter.java
+++ async-data/src/main/java/com/nextfaze/asyncdata/DataAdapter.java
@@ -1,12 +1,19 @@
 package com.nextfaze.asyncdata;
 
+import android.database.DataSetObserver;
 import android.widget.BaseAdapter;
 import lombok.NonNull;
 import lombok.experimental.Accessors;
 
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 /** Presents the contents of a {@link Data} instance, and responds to change events. */
 @Accessors(prefix = "m")
-public abstract class DataAdapter<T> extends BaseAdapter implements DisposableListAdapter {
+public abstract class DataAdapter<T> extends BaseAdapter {
+
+    @NonNull
+    private final Set<DataSetObserver> mDataObservers = new CopyOnWriteArraySet<>();
 
     @NonNull
     private final Data<T> mData;
@@ -21,17 +28,11 @@ public abstract class DataAdapter<T> extends BaseAdapter implements DisposableLi
 
     public DataAdapter(@NonNull Data<T> data) {
         mData = data;
-        mData.registerDataObserver(mDataObserver);
     }
 
     @NonNull
     public final Data<T> getData() {
         return mData;
-    }
-
-    @Override
-    public final void dispose() {
-        mData.unregisterDataObserver(mDataObserver);
     }
 
     @Override
@@ -42,6 +43,22 @@ public abstract class DataAdapter<T> extends BaseAdapter implements DisposableLi
     @Override
     public final T getItem(int position) {
         return mData.get(position, Data.FLAG_PRESENTATION);
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+        super.registerDataSetObserver(observer);
+        if (mDataObservers.add(observer) && mDataObservers.size() == 1) {
+            mData.registerDataObserver(mDataObserver);
+        }
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+        super.unregisterDataSetObserver(observer);
+        if (mDataObservers.remove(observer) && mDataObservers.size() == 0) {
+            mData.unregisterDataObserver(mDataObserver);
+        }
     }
 
     /** By default, simply returns the position, since a data item does not intrinsically have a {@code long} ID. */
