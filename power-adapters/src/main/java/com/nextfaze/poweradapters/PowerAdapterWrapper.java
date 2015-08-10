7ca76a44e267935b93fa9249a6276ff8c7ff751d
Ben Tilbrook <btilbrook@nextfaze.com>
2015-08-10 10:20:48 +0930

    Add PowerAdapterWrapper

---
+++ power-adapters/src/main/java/com/nextfaze/poweradapters/PowerAdapterWrapper.java
@@ -0,0 +1,120 @@
+package com.nextfaze.poweradapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import lombok.NonNull;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class PowerAdapterWrapper extends AbstractPowerAdapter {
+
+    @NonNull
+    private final Set<DataObserver> mDataSetObservers = new CopyOnWriteArraySet<>();
+
+    @NonNull
+    private final PowerAdapter mAdapter;
+
+    @NonNull
+    private final DataObserver mDataSetObserver = new DataObserver() {
+        @Override
+        public void onChanged() {
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+            notifyItemRangeChanged(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            notifyItemRangeInserted(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            notifyItemRangeRemoved(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+            notifyItemRangeMoved(fromPosition, toPosition, itemCount);
+        }
+    };
+
+    public PowerAdapterWrapper(@NonNull PowerAdapter adapter) {
+        mAdapter = adapter;
+    }
+
+    @NonNull
+    public final PowerAdapter getAdapter() {
+        return mAdapter;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAdapter.getItemCount();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return mAdapter.hasStableIds();
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return mAdapter.getViewTypeCount();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return mAdapter.getItemId(position);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mAdapter.getItemViewType(position);
+    }
+
+    @Override
+    @NonNull
+    public Metadata getItemMetadata(int position) {
+        return mAdapter.getItemMetadata(position);
+    }
+
+    @Override
+    @NonNull
+    public View newView(@NonNull ViewGroup parent, int itemViewType) {
+        return mAdapter.newView(parent, itemViewType);
+    }
+
+    @Override
+    public void bindView(@NonNull View view, int position) {
+        mAdapter.bindView(view, position);
+    }
+
+    @Override
+    public void registerDataObserver(@NonNull DataObserver dataObserver) {
+        super.registerDataObserver(dataObserver);
+        if (mDataSetObservers.add(dataObserver) && mDataSetObservers.size() == 1) {
+            mAdapter.registerDataObserver(mDataSetObserver);
+            onFirstObserverRegistered();
+        }
+    }
+
+    @Override
+    public void unregisterDataObserver(@NonNull DataObserver dataObserver) {
+        super.unregisterDataObserver(dataObserver);
+        if (mDataSetObservers.remove(dataObserver) && mDataSetObservers.size() == 0) {
+            mAdapter.unregisterDataObserver(mDataSetObserver);
+            onLastObserverUnregistered();
+        }
+    }
+
+    protected void onFirstObserverRegistered() {
+    }
+
+    protected void onLastObserverUnregistered() {
+    }
+}
