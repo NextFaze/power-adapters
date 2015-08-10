bac93d1a21b242308124c8ef512c614bf4743e97
Ben Tilbrook <btilbrook@nextfaze.com>
2015-08-10 11:03:22 +0930

    Create BindingPowerAdapter and DataBindingAdapter

---
+++ power-adapters/src/main/java/com/nextfaze/poweradapters/BindingPowerAdapter.java
@@ -0,0 +1,74 @@
+package com.nextfaze.poweradapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import lombok.NonNull;
+import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.IdentityHashMap;
+
+@Accessors(prefix = "m")
+public abstract class BindingPowerAdapter extends AbstractPowerAdapter {
+
+    @NonNull
+    private final ArrayList<Binder> mBinders;
+
+    @NonNull
+    private final IdentityHashMap<Binder, Integer> mIndexes = new IdentityHashMap<Binder, Integer>();
+
+    @NonNull
+    private final Mapper mMapper;
+
+    public BindingPowerAdapter(@NonNull Mapper mapper) {
+        mBinders = new ArrayList<Binder>(mapper.getAllBinders());
+        mMapper = mapper;
+    }
+
+    @NonNull
+    protected abstract Object getItem(int position);
+
+    @NonNull
+    @Override
+    public View newView(@NonNull ViewGroup parent, int itemViewType) {
+        if (itemViewType >= mBinders.size()) {
+            throw new AssertionError();
+        }
+        Binder binder = mBinders.get(itemViewType);
+        if (binder == null) {
+            throw new AssertionError();
+        }
+        return binder.newView(parent);
+    }
+
+    @Override
+    public void bindView(@NonNull View view, int position) {
+        Object item = getItem(position);
+        Binder binder = mMapper.getBinder(item, position);
+        if (binder != null) {
+            binder.bindView(item, view, position);
+        }
+    }
+
+    @Override
+    public final int getViewTypeCount() {
+        return super.getViewTypeCount() + mBinders.size();
+    }
+
+    @Override
+    public final int getItemViewType(int position) {
+        Object item = getItem(position);
+        Binder binder = mMapper.getBinder(item, position);
+        if (binder != null) {
+            // Cache index of each binder to avoid linear search each time.
+            Integer index = mIndexes.get(binder);
+            if (index == null) {
+                index = mBinders.indexOf(binder);
+                mIndexes.put(binder, index);
+            }
+            // Offset type by inner type count, otherwise we could get collisions.
+            return super.getViewTypeCount() + index;
+        }
+        throw new AssertionError();
+    }
+}
---
+++ power-adapters/src/main/java/com/nextfaze/poweradapters/DataBindingAdapter.java
@@ -0,0 +1,44 @@
+package com.nextfaze.poweradapters;
+
+import com.nextfaze.asyncdata.Data;
+import lombok.NonNull;
+
+public final class DataBindingAdapter extends BindingPowerAdapter {
+
+    @NonNull
+    private final com.nextfaze.asyncdata.DataObserver mDataObserver = new com.nextfaze.asyncdata.DataObserver() {
+        @Override
+        public void onChange() {
+            notifyDataSetChanged();
+        }
+    };
+
+    @NonNull
+    private final Data<?> mData;
+
+    public DataBindingAdapter(@NonNull Data<?> data, @NonNull Mapper mapper) {
+        super(mapper);
+        mData = data;
+    }
+
+    @NonNull
+    @Override
+    protected Object getItem(int position) {
+        return mData.get(position, Data.FLAG_PRESENTATION);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    @Override
+    protected void onFirstObserverRegistered() {
+        mData.registerDataObserver(mDataObserver);
+    }
+
+    @Override
+    protected void onLastObserverUnregistered() {
+        mData.unregisterDataObserver(mDataObserver);
+    }
+}
