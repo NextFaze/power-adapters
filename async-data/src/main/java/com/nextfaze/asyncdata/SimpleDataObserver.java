df2c22192dcc2b4ae39556f9686fa4dbb207fe89
Ben Tilbrook <btilbrook@nextfaze.com>
2015-08-12 12:49:58 +0930

    Add fine-grained data change notification methods to DataObserver, and a SimpleDataObserver for convenience

---
+++ async-data/src/main/java/com/nextfaze/asyncdata/SimpleDataObserver.java
@@ -0,0 +1,23 @@
+package com.nextfaze.asyncdata;
+
+public class SimpleDataObserver implements DataObserver {
+    @Override
+    public void onChange() {
+    }
+
+    @Override
+    public void onItemRangeChanged(int positionStart, int itemCount) {
+    }
+
+    @Override
+    public void onItemRangeInserted(int positionStart, int itemCount) {
+    }
+
+    @Override
+    public void onItemRangeRemoved(int positionStart, int itemCount) {
+    }
+
+    @Override
+    public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+    }
+}
