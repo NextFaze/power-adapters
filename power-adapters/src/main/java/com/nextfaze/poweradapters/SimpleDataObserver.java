9302f84d4f871cb55b2f00db36cddef030a119f3
Ben Tilbrook <btilbrook@nextfaze.com>
2015-09-02 10:56:06 +0930

    Make SimpleDataObserver abstract, and onChange() abstract

--- power-adapters/src/main/java/com/nextfaze/poweradapters/SimpleDataObserver.java
+++ power-adapters/src/main/java/com/nextfaze/poweradapters/SimpleDataObserver.java
@@ -1,10 +1,9 @@
 package com.nextfaze.poweradapters;
 
 /** Forwards fine-grained calls to {@link DataObserver#onChanged()} by default. */
-public class SimpleDataObserver implements DataObserver {
+public abstract class SimpleDataObserver implements DataObserver {
     @Override
-    public void onChanged() {
-    }
+    public abstract void onChanged();
 
     @Override
     public void onItemRangeChanged(int positionStart, int itemCount) {
