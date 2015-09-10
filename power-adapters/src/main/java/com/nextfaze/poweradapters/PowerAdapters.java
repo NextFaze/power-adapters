aba4c0cb6880ec2040b003147e70f0ae97b8cbe2
Ben Tilbrook <btilbrook@nextfaze.com>
2015-09-10 11:27:21 +0930

    Move decorator static methods to Decorators util class, and add an "empty" decorator instance

--- power-adapters/src/main/java/com/nextfaze/poweradapters/PowerAdapters.java
+++ power-adapters/src/main/java/com/nextfaze/poweradapters/PowerAdapters.java
@@ -45,25 +45,4 @@ public final class PowerAdapters {
         return new ConcatAdapter(powerAdapters);
     }
 
-    @CheckResult
-    @NonNull
-    public static PowerAdapter decorate(@NonNull PowerAdapter adapter, @NonNull Decorator... decorators) {
-        if (decorators.length == 0) {
-            return adapter;
-        }
-        for (Decorator decorator : decorators) {
-            adapter = decorator.decorate(adapter);
-        }
-        return adapter;
-    }
-
-    @CheckResult
-    @NonNull
-    public static PowerAdapter decorate(@NonNull PowerAdapter adapter,
-                                        @NonNull Iterable<? extends Decorator> decorators) {
-        for (Decorator decorator : decorators) {
-            adapter = decorator.decorate(adapter);
-        }
-        return adapter;
-    }
 }
