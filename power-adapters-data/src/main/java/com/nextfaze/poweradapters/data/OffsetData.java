99452a3ed02e1ec9a27e570945afcb644c5585a3
Ben Tilbrook <btilbrook@nextfaze.com>
2016-04-15 13:20:21 +0930

    Implement OffsetData and tests

--- power-adapters-data/src/main/java/com/nextfaze/poweradapters/data/OffsetData.java
+++ power-adapters-data/src/main/java/com/nextfaze/poweradapters/data/OffsetData.java
@@ -2,6 +2,9 @@ package com.nextfaze.poweradapters.data;
 
 import lombok.NonNull;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 final class OffsetData<T> extends DataWrapper<T> {
 
     @NonNull
@@ -12,16 +15,47 @@ final class OffsetData<T> extends DataWrapper<T> {
     public OffsetData(@NonNull Data<? extends T> data, int offset) {
         super(data);
         mData = data;
-        mOffset = offset;
+        mOffset = max(0, offset);
     }
 
     @NonNull
     @Override
     public T get(int position, int flags) {
-        return mData.get(position, flags);
+        return mData.get(position + mOffset, flags);
     }
 
-    // TODO: Offset notifications.
+    @Override
+    public int size() {
+        return super.size() - mOffset;
+    }
 
-    // TODO: Override size() to reduce total, subtracting offset.
+    @Override
+    protected void forwardItemRangeChanged(int innerPositionStart, int innerItemCount) {
+        if (innerPositionStart + innerItemCount > mOffset) {
+            notifyItemRangeChanged(max(0, innerPositionStart - mOffset),
+                    min(innerItemCount, innerItemCount - mOffset + innerPositionStart));
+        }
+    }
+
+    @Override
+    protected void forwardItemRangeInserted(int innerPositionStart, int innerItemCount) {
+        if (innerPositionStart + innerItemCount > mOffset) {
+            notifyItemRangeInserted(max(0, innerPositionStart - mOffset),
+                    min(innerItemCount, innerItemCount - mOffset + innerPositionStart));
+        }
+    }
+
+    @Override
+    protected void forwardItemRangeRemoved(int innerPositionStart, int innerItemCount) {
+        if (innerPositionStart + innerItemCount > mOffset) {
+            notifyItemRangeRemoved(max(0, innerPositionStart - mOffset),
+                    min(innerItemCount, innerItemCount - mOffset + innerPositionStart));
+        }
+    }
+
+    @Override
+    protected void forwardItemRangeMoved(int innerFromPosition, int innerToPosition, int innerItemCount) {
+        // TODO: Fine-grained notifications.
+        notifyDataChanged();
+    }
 }
--- power-adapters-data/src/test/java/com/nextfaze/poweradapters/data/OffsetDataTest.java
+++ power-adapters-data/src/test/java/com/nextfaze/poweradapters/data/OffsetDataTest.java
@@ -2,13 +2,18 @@ package com.nextfaze.poweradapters.data;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.truth.Truth.assertThat;
 import static java.util.Collections.addAll;
+import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
@@ -20,10 +25,93 @@ public final class OffsetDataTest {
     private FakeData<String> mData;
     private Data<String> mOffsetData;
 
+    @Mock
+    private DataObserver mDataObserver;
+
     @Before
     public void setUp() throws Exception {
         mData = new FakeData<>();
         addAll(mData, "a", "bc", "def", "ghij", "klmno", "pqrstu", "vwxyz12");
         mOffsetData = new OffsetData<>(mData, 5);
+        mOffsetData.registerDataObserver(mDataObserver);
+    }
+
+    @Test
+    public void negativeOffsetClamped() {
+        assertThat(new OffsetData<>(mData, -10))
+                .containsExactly("a", "bc", "def", "ghij", "klmno", "pqrstu", "vwxyz12").inOrder();
+    }
+
+    @Test
+    public void reducedSize() {
+        assertThat(mOffsetData).hasSize(2);
+    }
+
+    @Test
+    public void clippedContents() {
+        assertOffsetDataClippedContents();
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getOutOfBoundsThrows() {
+        mOffsetData.get(3);
+    }
+
+    @Test
+    public void outOfBoundsChangeDropped() {
+        mData.set(4, "foo");
+        assertOffsetDataClippedContents();
+        verifyZeroInteractions(mDataObserver);
+    }
+
+    @Test
+    public void outOfBoundsInsertDropped() {
+        mData.add(1, "foo");
+        assertThat(mOffsetData).containsExactly("klmno", "pqrstu", "vwxyz12").inOrder();
+        verifyZeroInteractions(mDataObserver);
+    }
+
+    @Test
+    public void outOfBoundsRemoveDropped() {
+        mData.remove("def");
+        assertThat(mOffsetData).containsExactly("vwxyz12").inOrder();
+        verifyZeroInteractions(mDataObserver);
+    }
+
+    @Test
+    public void outOfBoundsMoveDropped() {
+        // TODO: Check that a move that's entirely out of bounds results in no notification.
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void boundaryStraddlingChangeClipped() {
+        mData.setNotificationsEnabled(false);
+        for (int i = 0; i < 3; i++) {
+            mData.set(3, "x");
+        }
+        mData.notifyItemRangeChanged(3, 3);
+        verify(mDataObserver).onItemRangeChanged(0, 1);
+        verifyNoMoreInteractions(mDataObserver);
+    }
+
+    @Test
+    public void boundaryStraddlingInsertClipped() {
+        mData.addAll(2, newArrayList("x", "y", "z", "w"));
+        assertThat(mOffsetData).containsExactly("w", "def", "ghij", "klmno", "pqrstu", "vwxyz12").inOrder();
+        verify(mDataObserver).onItemRangeInserted(0, 1);
+        verifyNoMoreInteractions(mDataObserver);
+    }
+
+    @Test
+    public void boundaryStraddlingRemoveClipped() {
+        mData.clear();
+        assertThat(mOffsetData).isEmpty();
+        verify(mDataObserver).onItemRangeRemoved(0, 2);
+        verifyNoMoreInteractions(mDataObserver);
+    }
+
+    private void assertOffsetDataClippedContents() {
+        assertThat(mOffsetData).containsExactly("pqrstu", "vwxyz12").inOrder();
     }
 }
