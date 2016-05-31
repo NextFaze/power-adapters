155e118b2cc8bb972f9bfad3cddae7aa9a9e613d
Ben Tilbrook <btilbrook@nextfaze.com>
2016-05-31 13:32:11 +0930

    Add AbstractConditionTest

---
+++ power-adapters/src/test/java/com/nextfaze/poweradapters/AbstractConditionTest.java
@@ -0,0 +1,84 @@
+package com.nextfaze.poweradapters;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public final class AbstractConditionTest {
+
+    @Rule
+    public MockitoRule mMockito = MockitoJUnit.rule();
+
+    private AbstractCondition mCondition;
+
+    @Before
+    public void setUp() throws Exception {
+        mCondition = spy(AbstractCondition.class);
+    }
+
+    @Test
+    public void observerRegistration() {
+        Observer observer = mock(Observer.class);
+        mCondition.registerObserver(observer);
+        assertThat(mCondition.getObserverCount()).isEqualTo(1);
+        mCondition.notifyChanged();
+        verify(observer).onChanged();
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void observerUnregistration() {
+        Observer observer = mock(Observer.class);
+        mCondition.registerObserver(observer);
+        mCondition.unregisterObserver(observer);
+        assertThat(mCondition.getObserverCount()).isEqualTo(0);
+        mCondition.notifyChanged();
+        verifyZeroInteractions(observer);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void doubleRegistrationThrows() {
+        Observer observer = mock(Observer.class);
+        mCondition.registerObserver(observer);
+        mCondition.registerObserver(observer);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void doubleUnregistrationThrows() {
+        Observer observer = mock(Observer.class);
+        mCondition.registerObserver(observer);
+        mCondition.unregisterObserver(observer);
+        mCondition.unregisterObserver(observer);
+    }
+
+    @Test
+    public void firstObserverRegisteredWasInvokedForFirstObserverOnly() {
+        Observer observer = mock(Observer.class);
+        Observer observer2 = mock(Observer.class);
+        mCondition.registerObserver(observer);
+        mCondition.registerObserver(observer2);
+        verify(mCondition).onFirstObserverRegistered();
+        verifyNoMoreInteractions(mCondition);
+    }
+
+    @Test
+    public void lastObserverUnregisteredWasInvokedForLastObserverOnly() {
+        Observer observer = mock(Observer.class);
+        Observer observer2 = mock(Observer.class);
+        mCondition.registerObserver(observer);
+        mCondition.registerObserver(observer2);
+        mCondition.unregisterObserver(observer);
+        mCondition.unregisterObserver(observer2);
+        verify(mCondition).onLastObserverUnregistered();
+    }
+}
