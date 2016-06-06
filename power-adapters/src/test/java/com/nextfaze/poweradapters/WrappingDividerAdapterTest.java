aba34b293ec7b8305ce75b200cf5ded341aeb0c0
Ben Tilbrook <btilbrook@nextfaze.com>
2016-06-06 11:27:14 +0930

    Add WrappingDividerAdapter tests, and fix/clean up WrapperDividerAdapter

---
+++ power-adapters/src/test/java/com/nextfaze/poweradapters/WrappingDividerAdapterTest.java
@@ -0,0 +1,222 @@
+package com.nextfaze.poweradapters;
+
+import android.support.annotation.LayoutRes;
+import lombok.NonNull;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.nextfaze.poweradapters.DividerAdapterBuilder.EmptyPolicy.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public final class WrappingDividerAdapterTest {
+
+    @LayoutRes
+    private static final int RESOURCE = 0;
+
+    @Rule
+    public MockitoRule mMockito = MockitoJUnit.rule();
+
+    @Mock
+    private DataObserver mObserver;
+
+    private FakeAdapter mFakeAdapter;
+    private PowerAdapter mDividerAdapter;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    private void configure(int count) {
+        configure(count, SHOW_NOTHING);
+    }
+
+    private void configure(int count, @NonNull DividerAdapterBuilder.EmptyPolicy emptyPolicy) {
+        mFakeAdapter = new FakeAdapter(count);
+        mDividerAdapter = new DividerAdapterBuilder()
+                .innerResource(RESOURCE)
+                .outerResource(RESOURCE)
+                .emptyPolicy(emptyPolicy)
+                .build(mFakeAdapter);
+        mDividerAdapter.registerDataObserver(new VerifyingObserver(mDividerAdapter));
+        mDividerAdapter.registerDataObserver(mObserver);
+    }
+
+    @Test
+    public void itemCountGreaterThanOne() {
+        configure(5, SHOW_LEADING);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(5);
+    }
+
+    @Test
+    public void itemCountOne() {
+        configure(1, SHOW_LEADING);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void itemCountZeroShowLeading() {
+        configure(0, SHOW_LEADING);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void itemCountZeroShowTrailing() {
+        configure(0, SHOW_TRAILING);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void itemCountZeroShowLeadingAndTrailing() {
+        configure(0, SHOW_LEADING_AND_TRAILING);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(2);
+    }
+
+    @Test
+    public void removeToEmptyShowNothing() {
+        configure(5, SHOW_NOTHING);
+        mFakeAdapter.clear();
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(0);
+        verify(mObserver).onItemRangeRemoved(0, 5);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void removeToEmptyShowLeading() {
+        configure(5, SHOW_LEADING);
+        mFakeAdapter.clear();
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(1);
+        verify(mObserver).onItemRangeRemoved(0, 5);
+        verify(mObserver).onItemRangeInserted(0, 1);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void removeToEmptyShowTrailing() {
+        configure(5, SHOW_TRAILING);
+        mFakeAdapter.clear();
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(1);
+        verify(mObserver).onItemRangeRemoved(0, 5);
+        verify(mObserver).onItemRangeInserted(0, 1);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void removeToEmptyShowLeadingAndTrailing() {
+        configure(5, SHOW_LEADING_AND_TRAILING);
+        mFakeAdapter.clear();
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(2);
+        verify(mObserver).onItemRangeRemoved(0, 5);
+        verify(mObserver).onItemRangeInserted(0, 1);
+        verify(mObserver).onItemRangeInserted(1, 1);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void removeToNonEmptyFromStartRange() {
+        configure(15);
+        mFakeAdapter.remove(0, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(10);
+        verify(mObserver).onItemRangeRemoved(0, 5);
+        verify(mObserver).onItemRangeChanged(0, 1);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void removeToNonEmptyFromEndRange() {
+        configure(15);
+        mFakeAdapter.remove(10, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(10);
+        verify(mObserver).onItemRangeRemoved(10, 5);
+        verify(mObserver).onItemRangeChanged(9, 1);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void removeToNonEmptyFromMidRange() {
+        configure(15);
+        mFakeAdapter.remove(5, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(10);
+        verify(mObserver).onItemRangeRemoved(5, 5);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void insertFromEmptyShowNothing() {
+        configure(0, SHOW_NOTHING);
+        mFakeAdapter.insert(0, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(5);
+        verify(mObserver).onItemRangeInserted(0, 5);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void insertFromEmptyShowLeading() {
+        configure(0, SHOW_LEADING);
+        mFakeAdapter.insert(0, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(5);
+        verify(mObserver).onItemRangeRemoved(0, 1);
+        verify(mObserver).onItemRangeInserted(0, 5);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void insertFromEmptyShowTrailing() {
+        configure(0, SHOW_TRAILING);
+        mFakeAdapter.insert(0, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(5);
+        verify(mObserver).onItemRangeRemoved(0, 1);
+        verify(mObserver).onItemRangeInserted(0, 5);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void insertFromEmptyShowLeadingAndTrailing() {
+        configure(0, SHOW_LEADING_AND_TRAILING);
+        mFakeAdapter.insert(0, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(5);
+        verify(mObserver).onItemRangeRemoved(0, 1);
+        verify(mObserver).onItemRangeRemoved(1, 1);
+        verify(mObserver).onItemRangeInserted(0, 5);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void insertFromNonEmptyToStartRange() {
+        configure(10);
+        mFakeAdapter.insert(0, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(15);
+        verify(mObserver).onItemRangeInserted(0, 5);
+        verify(mObserver).onItemRangeChanged(5, 1);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void insertFromNonEmptyToEndRange() {
+        configure(10);
+        mFakeAdapter.insert(10, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(15);
+        verify(mObserver).onItemRangeInserted(10, 5);
+        verify(mObserver).onItemRangeChanged(9, 1);
+        verifyNoMoreInteractions(mObserver);
+    }
+
+    @Test
+    public void insertFromNonEmptyToMidRange() {
+        configure(10);
+        mFakeAdapter.insert(5, 5);
+        assertThat(mDividerAdapter.getItemCount()).isEqualTo(15);
+        verify(mObserver).onItemRangeInserted(5, 5);
+        verifyNoMoreInteractions(mObserver);
+    }
+}
