29aca2aa94159bd4a513721daf8e6f5fc260f42c
Ben Tilbrook <btilbrook@nextfaze.com>
2016-06-20 14:36:01 +0930

    Add SortData and tests

---
+++ power-adapters-data/src/main/java/com/nextfaze/poweradapters/data/SortData.java
@@ -0,0 +1,168 @@
+package com.nextfaze.poweradapters.data;
+
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+final class SortData<T> extends DataWrapper<T> {
+
+    @NonNull
+    private final Data<? extends T> mData;
+
+    @NonNull
+    private final Comparator<? super T> mComparator;
+
+    @NonNull
+    private final Index mIndex = new Index();
+
+    SortData(@NonNull Data<? extends T> data, @NonNull Comparator<? super T> comparator) {
+        super(data);
+        mData = data;
+        mComparator = comparator;
+    }
+
+    @NonNull
+    @Override
+    public T get(int position, int flags) {
+        return mData.get(mIndex.outerToInner(position));
+    }
+
+    @Override
+    public int size() {
+        if (getDataObserverCount() <= 0) {
+            return mData.size();
+        }
+        return mIndex.size();
+    }
+
+    @Override
+    protected void onFirstDataObserverRegistered() {
+        super.onFirstDataObserverRegistered();
+        rebuild();
+    }
+
+    @Override
+    protected void forwardChanged() {
+        rebuild();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    protected void forwardItemRangeChanged(int innerPositionStart, int innerItemCount) {
+        for (int innerPosition = innerPositionStart; innerPosition < innerPositionStart + innerItemCount; innerPosition++) {
+            T t = mData.get(innerPosition);
+            int oldOuterPosition = mIndex.remove(innerPosition);
+            notifyItemRemoved(oldOuterPosition);
+            int newOuterPosition = findOuterPositionForValue(t);
+            mIndex.insert(newOuterPosition, innerPosition);
+            notifyItemInserted(newOuterPosition);
+        }
+    }
+
+    @Override
+    protected void forwardItemRangeInserted(int innerPositionStart, int innerItemCount) {
+        mIndex.shift(innerPositionStart, +innerItemCount);
+        for (int innerPosition = innerPositionStart; innerPosition < innerPositionStart + innerItemCount; innerPosition++) {
+            T value = mData.get(innerPosition);
+            int outerPosition = findOuterPositionForValue(value);
+            mIndex.insert(outerPosition, innerPosition);
+            notifyItemInserted(outerPosition);
+        }
+    }
+
+    @Override
+    protected void forwardItemRangeRemoved(int innerPositionStart, int innerItemCount) {
+        for (int innerPosition = innerPositionStart; innerPosition < innerPositionStart + innerItemCount; innerPosition++) {
+            notifyItemRemoved(mIndex.remove(innerPosition));
+        }
+        mIndex.shift(innerPositionStart, -innerItemCount);
+    }
+
+    @Override
+    protected void forwardItemRangeMoved(int innerFromPosition, int innerToPosition, int innerItemCount) {
+        // TODO: Support fine-grained notifications for moves.
+        rebuild();
+        notifyDataSetChanged();
+    }
+
+    private void rebuild() {
+        mIndex.clear();
+        int size = mData.size();
+        for (int innerPosition = 0; innerPosition < size; innerPosition++) {
+            T value = mData.get(innerPosition);
+            int outerPosition = findOuterPositionForValue(value);
+            mIndex.put(outerPosition, innerPosition);
+        }
+    }
+
+    private int findOuterPositionForValue(T value) {
+        final int size = mIndex.size();
+        int lo = 0;
+        int hi = size - 1;
+        while (lo <= hi) {
+            final int mid = (lo + hi) >>> 1;
+            final T midVal = mData.get(mIndex.outerToInner(mid));
+            int result = mComparator.compare(midVal, value);
+            if (result < 0) {
+                lo = mid + 1;
+            } else if (result > 0) {
+                hi = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(~lo) - 1;
+    }
+
+    private static final class Index {
+
+        @NonNull
+        private final ArrayList<Integer> mInnerPositions = new ArrayList<>();
+
+        int size() {
+            return mInnerPositions.size();
+        }
+
+        int outerToInner(int outerPosition) {
+            return mInnerPositions.get(outerPosition);
+        }
+
+        void put(int outerPosition, int innerPosition) {
+            mInnerPositions.add(outerPosition, innerPosition);
+        }
+
+        void insert(int outerPosition, int innerPosition) {
+            mInnerPositions.add(outerPosition, innerPosition);
+        }
+
+        int remove(int innerPosition) {
+            // TODO: Avoid O(N) search.
+            int outerPosition = mInnerPositions.indexOf(innerPosition);
+            mInnerPositions.remove(outerPosition);
+            return outerPosition;
+        }
+
+        void shift(int innerPositionStart, int delta) {
+            if (delta > 0) {
+                for (int i = mInnerPositions.size() - 1; i >= 0; i--) {
+                    int innerPosition = mInnerPositions.get(i);
+                    if (innerPosition >= innerPositionStart) {
+                        mInnerPositions.set(i, innerPosition + delta);
+                    }
+                }
+            } else if (delta < 0) {
+                for (int i = 0; i < mInnerPositions.size(); i++) {
+                    int innerPosition = mInnerPositions.get(i);
+                    if (innerPosition >= innerPositionStart) {
+                        mInnerPositions.set(i, innerPosition + delta);
+                    }
+                }
+            }
+        }
+
+        void clear() {
+            mInnerPositions.clear();
+        }
+    }
+}
---
+++ power-adapters-data/src/test/java/com/nextfaze/poweradapters/data/SortDataTest.java
@@ -0,0 +1,166 @@
+package com.nextfaze.poweradapters.data;
+
+import com.google.common.collect.Ordering;
+import com.nextfaze.poweradapters.DataObserver;
+import lombok.NonNull;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.truth.Truth.assertThat;
+import static java.util.Collections.addAll;
+import static java.util.Collections.sort;
+import static org.mockito.Mockito.*;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public final class SortDataTest {
+
+    @Rule
+    public MockitoRule mMockito = MockitoJUnit.rule();
+
+    private FakeData<Integer> mFakeData;
+
+    private SortData<Integer> mSortData;
+
+    @Before
+    public void setUp() throws Exception {
+        mFakeData = new FakeData<>();
+        // 0, 5, 10, 15, 20, 25
+        addAll(mFakeData, 0, 10, 5, 15, 25, 20);
+        mSortData = new SortData<>(mFakeData, Ordering.natural());
+        mSortData.registerDataObserver(new VerifyingDataObserver(mSortData));
+    }
+
+    @Test
+    public void elementsSorted() {
+        assertContentsSorted();
+    }
+
+    private void assertContentsSorted() {
+        List<Integer> sortedFakeItems = newArrayList(mFakeData);
+        sort(sortedFakeItems);
+        System.out.println("Contents: " + newArrayList(mSortData));
+        assertThat(mSortData).containsExactlyElementsIn(sortedFakeItems).inOrder();
+    }
+
+    @Test
+    public void changeSingle1() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.change(1, 16);
+        assertContentsSorted();
+        verify(observer).onItemRangeRemoved(2, 1);
+        verify(observer).onItemRangeInserted(3, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void changeSingle2() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.change(3, 4);
+        assertContentsSorted();
+        verify(observer).onItemRangeRemoved(3, 1);
+        verify(observer).onItemRangeInserted(1, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void changeSingle3() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.change(4, 26);
+        assertContentsSorted();
+        verify(observer).onItemRangeRemoved(5, 1);
+        verify(observer).onItemRangeInserted(5, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void changeMultiple1() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.change(1, 18, 29, 1);
+        assertContentsSorted();
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer).onItemRangeRemoved(2, 1);
+        inOrder.verify(observer).onItemRangeInserted(3, 1);
+        inOrder.verify(observer).onItemRangeRemoved(1, 1);
+        inOrder.verify(observer).onItemRangeInserted(5, 1);
+        inOrder.verify(observer).onItemRangeRemoved(1, 1);
+        inOrder.verify(observer).onItemRangeInserted(1, 1);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void insertSingle() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.insert(3, 1);
+        assertContentsSorted();
+        verify(observer).onItemRangeInserted(1, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void insertMultiple1() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.insert(2, 1, 6, 17);
+        assertContentsSorted();
+        verify(observer).onItemRangeInserted(1, 1);
+        verify(observer).onItemRangeInserted(3, 1);
+        verify(observer).onItemRangeInserted(6, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void insertMultiple2() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.insert(0, 26, 3);
+        assertContentsSorted();
+        verify(observer).onItemRangeInserted(6, 1);
+        verify(observer).onItemRangeInserted(1, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void removeSingle() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.remove(1, 1);
+        assertContentsSorted();
+        verify(observer).onItemRangeRemoved(2, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void removeMultiple1() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.remove(0, 4);
+        assertContentsSorted();
+        verify(observer, times(3)).onItemRangeRemoved(0, 1);
+        verify(observer, times(1)).onItemRangeRemoved(1, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @Test
+    public void removeMultiple2() {
+        DataObserver observer = registerMockObserver();
+        mFakeData.remove(1, 3);
+        assertContentsSorted();
+        verify(observer).onItemRangeRemoved(2, 1);
+        verify(observer, times(2)).onItemRangeRemoved(1, 1);
+        verifyNoMoreInteractions(observer);
+    }
+
+    @NonNull
+    private DataObserver registerMockObserver() {
+        DataObserver observer = mock(DataObserver.class);
+        mSortData.registerDataObserver(observer);
+        return observer;
+    }
+}
