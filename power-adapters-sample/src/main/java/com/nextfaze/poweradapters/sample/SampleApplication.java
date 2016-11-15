882616515df7ca5f0340d3cc4517475bc4c2b8f9
Kane O'Riley <koriley@nextfaze.com>
2016-11-15 11:26:13 +1030

    Fix crash on pre-Lollipop devices when inflating FileView

--- power-adapters-sample/src/main/java/com/nextfaze/poweradapters/sample/SampleApplication.java
+++ power-adapters-sample/src/main/java/com/nextfaze/poweradapters/sample/SampleApplication.java
@@ -1,12 +1,15 @@
 package com.nextfaze.poweradapters.sample;
 
 import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
 import com.squareup.leakcanary.LeakCanary;
 
 public final class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         LeakCanary.install(this);
     }
 }
