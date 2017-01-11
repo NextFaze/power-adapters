5a131caeee349c4198efe25b4a6cfc1a5fb07633
Ben Tilbrook <btilbrook@nextfaze.com>
2017-01-12 09:51:34 +1030

    Add negative Data condition static methods

--- power-adapters-data/src/main/java/com/nextfaze/poweradapters/data/DataConditions.java
+++ power-adapters-data/src/main/java/com/nextfaze/poweradapters/data/DataConditions.java
@@ -26,6 +26,16 @@ public final class DataConditions {
     }
 
     @NonNull
+    public static Condition isNotEmpty(@NonNull Data<?> data) {
+        return data(data, new Predicate<Data<?>>() {
+            @Override
+            public boolean apply(Data<?> data) {
+                return !data.isEmpty();
+            }
+        });
+    }
+
+    @NonNull
     public static Condition isLoading(@NonNull Data<?> data) {
         return data(data, new Predicate<Data<?>>() {
             @Override
@@ -36,11 +46,31 @@ public final class DataConditions {
     }
 
     @NonNull
+    public static Condition isNotLoading(@NonNull Data<?> data) {
+        return data(data, new Predicate<Data<?>>() {
+            @Override
+            public boolean apply(Data<?> data) {
+                return !data.isLoading();
+            }
+        });
+    }
+
+    @NonNull
     public static Condition hasAvailableElements(@NonNull Data<?> data) {
         return data(data, new Predicate<Data<?>>() {
             @Override
             public boolean apply(Data<?> data) {
                 return data.available() > 0;
+            }
+        });
+    }
+
+    @NonNull
+    public static Condition hasNoAvailableElements(@NonNull Data<?> data) {
+        return data(data, new Predicate<Data<?>>() {
+            @Override
+            public boolean apply(Data<?> data) {
+                return data.available() == 0;
             }
         });
     }
