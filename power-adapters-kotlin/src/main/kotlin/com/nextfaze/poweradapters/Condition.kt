b553ecc08ec9cc76179176f09f5b2b3f8d59eb3a
Ben Tilbrook <btilbrook@nextfaze.com>
2017-04-07 08:55:54 +0930

    Add plus/minus assign operator overloads Kotlin extensions for Condition observers

--- power-adapters-kotlin/src/main/kotlin/com/nextfaze/poweradapters/Condition.kt
+++ power-adapters-kotlin/src/main/kotlin/com/nextfaze/poweradapters/Condition.kt
@@ -1,7 +1,8 @@
 package com.nextfaze.poweradapters
 
 infix fun Condition.and(condition: Condition): Condition = Condition.and(this, condition)
-
 infix fun Condition.or(condition: Condition): Condition = Condition.or(this, condition)
-
 infix fun Condition.xor(condition: Condition): Condition = Condition.xor(this, condition)
+
+operator fun Condition.plusAssign(observer: Observer) = registerObserver(observer)
+operator fun Condition.minusAssign(observer: Observer) = unregisterObserver(observer)
