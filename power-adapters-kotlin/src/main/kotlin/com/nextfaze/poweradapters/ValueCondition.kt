d026fc6de2fdc8b9bbfeb61722c7a21ca936b748
Ben Tilbrook <btilbrook@nextfaze.com>
2017-05-09 12:47:02 +0930

    Allow local property delegation to a ValueCondition

--- power-adapters-kotlin/src/main/kotlin/com/nextfaze/poweradapters/ValueCondition.kt
+++ power-adapters-kotlin/src/main/kotlin/com/nextfaze/poweradapters/ValueCondition.kt
@@ -2,5 +2,5 @@ package com.nextfaze.poweradapters
 
 import kotlin.reflect.KProperty
 
-operator fun ValueCondition.getValue(thisRef: Any, property: KProperty<*>): Boolean = get()
-operator fun ValueCondition.setValue(thisRef: Any, property: KProperty<*>, value: Boolean) = set(value)
+operator fun ValueCondition.getValue(thisRef: Any?, property: KProperty<*>): Boolean = get()
+operator fun ValueCondition.setValue(thisRef: Any?, property: KProperty<*>, value: Boolean) = set(value)
