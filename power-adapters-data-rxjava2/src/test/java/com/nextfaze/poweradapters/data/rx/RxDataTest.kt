28d7a9adde0aab5a70deec1b8bd3d919e43c5873
Ben Tilbrook <btilbrook@nextfaze.com>
2017-10-06 15:02:50 +1030

    Add more RxData tests #52

--- power-adapters-data-rxjava2/src/test/java/com/nextfaze/poweradapters/data/rx/RxDataTest.kt
+++ power-adapters-data-rxjava2/src/test/java/com/nextfaze/poweradapters/data/rx/RxDataTest.kt
@@ -2,6 +2,10 @@ package com.nextfaze.poweradapters.data.rx
 
 import com.nextfaze.poweradapters.data.FakeData
 import com.nextfaze.poweradapters.data.rxjava2.BuildConfig
+import com.nextfaze.poweradapters.rxjava2.ChangeEvent
+import com.nextfaze.poweradapters.rxjava2.InsertEvent
+import com.nextfaze.poweradapters.rxjava2.MoveEvent
+import com.nextfaze.poweradapters.rxjava2.RemoveEvent
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,19 +30,54 @@ class RxDataTest {
         testObserver.assertNotTerminated().assertValues(0, 1, 3, 2)
     }
 
+    @Test fun changes() {
+        val testObserver = RxData.changes(data).test()
+        data.add("a")
+        data[0] = "b"
+        testObserver.assertNotTerminated().assertValues(ChangeEvent(0, 1))
+    }
+
+    @Test fun inserts() {
+        val testObserver = RxData.inserts(data).test()
+        data.add("a")
+        testObserver.assertNotTerminated().assertValues(InsertEvent(0, 1))
+    }
+
+    @Test fun removes() {
+        val testObserver = RxData.removes(data).test()
+        data.add("a")
+        data.remove("a")
+        testObserver.assertNotTerminated().assertValues(RemoveEvent(0, 1))
+    }
+
+    @Test fun moves() {
+        val testObserver = RxData.moves(data).test()
+        data.addAll(listOf("a", "b", "c"))
+        data.move(1, 2, 1)
+        testObserver.assertNotTerminated().assertValues(MoveEvent(1, 2, 1))
+    }
+
     @Test fun loading() {
         val testObserver = RxData.loading(data).test()
         data.isLoading = true
         data.isLoading = false
-        testObserver.assertNotTerminated()
-        testObserver.assertValues(false, true, false)
+        testObserver.assertNotTerminated().assertValues(false, true, false)
     }
 
     @Test fun available() {
         data.setAvailable(0)
         val testObserver = RxData.available(data).test()
         data.setAvailable(5)
-        testObserver.assertNotTerminated()
-        testObserver.assertValues(0, 5)
+        testObserver.assertNotTerminated().assertValues(0, 5)
+    }
+
+    @Test fun errors() {
+        data.error(RuntimeException())
+        val testObserver = RxData.errors(data).test()
+        val e1 = RuntimeException()
+        val e2 = RuntimeException()
+        data.error(e1)
+        data.error(e2)
+        testObserver.assertNotTerminated().assertValues(e1, e2)
     }
 }
