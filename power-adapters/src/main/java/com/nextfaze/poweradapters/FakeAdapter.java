35574681844e0e31dab5d959843e6952cf148e03
Ben Tilbrook <btilbrook@nextfaze.com>
2017-10-08 10:50:39 +1030

    Move FakeAdapter to main source tree for access by tests

--- power-adapters/src/main/java/com/nextfaze/poweradapters/FakeAdapter.java
+++ power-adapters/src/main/java/com/nextfaze/poweradapters/FakeAdapter.java
@@ -1,6 +1,7 @@
 package com.nextfaze.poweradapters;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.view.View;
 import android.view.ViewGroup;
 import com.nextfaze.poweradapters.internal.NotificationType;
@@ -8,6 +9,7 @@ import com.nextfaze.poweradapters.internal.NotificationType;
 import static com.nextfaze.poweradapters.internal.Preconditions.checkNotNull;
 
 @SuppressWarnings("WeakerAccess")
+@VisibleForTesting
 public class FakeAdapter extends PowerAdapter {
 
     private int mItemCount;
