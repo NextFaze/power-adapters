cbbac2e431d90b2913a2d627adb8f1745ec13d36
Ben Tilbrook <btilbrook@nextfaze.com>
2017-10-17 14:59:12 +1030

    Add Kotlin top-level function viewFactory()

--- power-adapters-kotlin/src/main/java/com/nextfaze/poweradapters/ViewFactory.kt
+++ power-adapters-kotlin/src/main/java/com/nextfaze/poweradapters/ViewFactory.kt
@@ -1,7 +1,16 @@
 package com.nextfaze.poweradapters
 
+import android.support.annotation.LayoutRes
+import android.view.View
+import com.nextfaze.poweradapters.internal.AdapterUtils
+
 fun ViewFactory.toAdapter(): PowerAdapter = PowerAdapter.asAdapter(this)
 
 fun Iterable<ViewFactory>.toAdapter(): PowerAdapter = PowerAdapter.asAdapter(this)
 
 fun Collection<ViewFactory>.toAdapter(): PowerAdapter = PowerAdapter.asAdapter(this)
+
+@Suppress("UNCHECKED_CAST")
+fun <T : View> viewFactory(@LayoutRes layoutResource: Int, body: T.() -> Unit = {}) = ViewFactory {
+    (AdapterUtils.layoutInflater(it).inflate(layoutResource, it, false) as T).apply(body)
+}
